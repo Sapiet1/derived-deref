c1187fe013d98e0b1b97bafc85cfbae98b21dd81
Sapiet1 <3cvang24@gmail.com>
2023-08-15 12:10:08 -0500

    updated reference ergonomics

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! A crate for deriving the [`Deref`](https://doc.rust-lang.org/std/ops/trait.Deref.html)
-//! and [`DerefMut`](https://doc.rust-lang.org/std/ops/trait.DerefMut.html) traits from the standard
-//! library onto structs with at least one field. 
+//! and [`DerefMut`](https://doc.rust-lang.org/std/ops/trait.DerefMut.html) 
+//! traits from the standard library onto structs with at least one field. 
+//! Fields with references are passed directly.
 //!
 //! # Examples
 //!
@@ -31,6 +32,8 @@ use syn::{parse_macro_input, ItemStruct, Ident, Generics, Field, Fields, Index, 
 use quote::quote;
 use proc_macro2::TokenStream as TokenStream2;
 
+/// Derives the [`Deref`](https://doc.rust-lang.org/std/ops/trait.Deref.html)
+/// trait, passing the field directly if a reference type.
 #[proc_macro_derive(Deref, attributes(target))]
 pub fn derive_deref(input: TokenStream) -> TokenStream {
     // Creates the ItemStruct...
@@ -41,11 +44,14 @@ pub fn derive_deref(input: TokenStream) -> TokenStream {
     // ...to then get the desired field, one marked by `#[target]`.
     // However, if there's only one field, being marked is no longer required.
     match extract_field_parameters(item_struct.fields, "Deref") {
-        Ok((field_name, field_type)) => impl_deref(name, generics, field_name, Some(field_type)),
+        Ok((field_name, field_type, is_reference)) => impl_deref(name, generics, field_name, Some(field_type), is_reference),
         Err(error) => error,
     }
 }
 
+/// Derives the [`DerefMut`](https://doc.rust-lang.org/std/ops/trait.DerefMut.html) 
+/// trait, passing the field directly if a reference type. This will fail to
+/// compile if the chosen field is an immutable reference type.
 #[proc_macro_derive(DerefMut, attributes(target))]
 // Deriving for `DerefMut` is the same as with `Deref` with the excpetion that
 // `Target` does not need to be defined.
@@ -55,7 +61,7 @@ pub fn derive_deref_mut(input: TokenStream) -> TokenStream {
     let generics = item_struct.generics;
 
     match extract_field_parameters(item_struct.fields, "DerefMut") {
-        Ok((field_name, _)) => impl_deref(name, generics, field_name, None),
+        Ok((field_name, _, is_reference)) => impl_deref(name, generics, field_name, None, is_reference),
         Err(error) => error,
     }
 }
@@ -92,21 +98,29 @@ fn get_field(fields: Punctuated<Field, Comma>) -> Result<(usize, Field), TokenSt
     }
 }
 
-fn extract_field_parameters(fields: Fields, trait_name: &str) -> Result<(TokenStream2, Type), TokenStream> {
+fn extract_field_parameters(fields: Fields, trait_name: &str) -> Result<(TokenStream2, Type, Option<bool>), TokenStream> {
     match fields {
         Fields::Named(fields) => {
             let (_, field) = get_field(fields.named)?;
             let field_name = field.ident.unwrap();
-            let field_type = field.ty;
+            let (field_type, is_mut_reference) = match field.ty {
+                Type::Reference(reference_type) if reference_type.mutability.is_some() => (*reference_type.elem, Some(true)),
+                Type::Reference(reference_type) => (*reference_type.elem, Some(false)),
+                field_type => (field_type, None),
+            };
 
-            Ok((quote! { #field_name }, field_type))
+            Ok((quote! { #field_name }, field_type, is_mut_reference))
         },
         Fields::Unnamed(fields) => {
             let (field_index, field) = get_field(fields.unnamed)?;
             let field_index = Index::from(field_index);
-            let field_type = field.ty;
+            let (field_type, is_mut_reference) = match field.ty {
+                Type::Reference(reference_type) if reference_type.mutability.is_some() => (*reference_type.elem, Some(true)),
+                Type::Reference(reference_type) => (*reference_type.elem, Some(false)),
+                field_type => (field_type, None),
+            };
 
-            Ok((quote! { #field_index }, field_type))
+            Ok((quote! { #field_index }, field_type, is_mut_reference))
         },
         Fields::Unit => {
             let error = &format!("unable to implement `{}` trait for struct of no fields", trait_name)[..];
@@ -116,34 +130,46 @@ fn extract_field_parameters(fields: Fields, trait_name: &str) -> Result<(TokenSt
     }
 }
 
-// Only whenever there is no need for `field_type` does it mean `Deref` is 
-// being implemented with its mutable counterpart.
 fn impl_deref(
     struct_name: Ident,
     struct_generics: Generics,
     field_name: TokenStream2,
+    // Only whenever there is no need for `field_type` does it mean `Deref` is 
+    // being implemented with its mutable counterpart.
     field_type: Option<Type>,
+    // For if the field is a reference: `Some` if it is and `None` otherwise.
+    // The boolean is `true` when it is mutable and `false` otherwise.
+    is_mut_reference: Option<bool>,
 ) -> TokenStream 
 {
     let (impl_generics, type_generics, where_clause) = struct_generics.split_for_impl();
 
     match field_type {
         Some(field_type) => {
+            // If not a reference, "&" is passed. If it is, nothing is instead. 
+            let reference = is_mut_reference.map_or_else(|| Some(quote!(&)), |_| None);
+            
             quote! {
                 impl #impl_generics std::ops::Deref for #struct_name #type_generics #where_clause {
                     type Target = #field_type;
 
                     fn deref(&self) -> &Self::Target {
-                        &self.#field_name
+                        #reference self.#field_name
                     }
                 }
             }
         },
         None => {
+            let reference = match is_mut_reference {
+                Some(true) => None,
+                Some(false) => return quote! { compile_error!("field is unable to be of an immutable reference"); }.into(),
+                None => Some(quote!(&mut)),
+            };
+            
             quote! {
                 impl #impl_generics std::ops::DerefMut for #struct_name #type_generics #where_clause {
                     fn deref_mut(&mut self) -> &mut Self::Target {
-                        &mut self.#field_name
+                        #reference self.#field_name
                     }
                 }
             }
